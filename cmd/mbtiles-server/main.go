67bf74589e3396c1c815df84760e1e32dd589d76
Tom Payne <twpayne@gmail.com>
2017-06-26 21:38:25 +0200

    Add simple viewer based on OpenLayers

--- cmd/mbtiles-server/main.go
+++ cmd/mbtiles-server/main.go
@@ -2,19 +2,61 @@ package main
 
 import (
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/twpayne/go-mbtiles"
+)
+
+var (
+	indexHTML = template.Must(template.New("index.html").Parse(`<html>
+	<head>
+		<title>mbtiles-server</title>
+		<link rel="stylesheet" href="https://openlayers.org/en/v4.2.0/css/ol.css" type="text/css">
+		<script src="https://openlayers.org/en/v4.2.0/build/ol.js"></script>
+	</head>
+	<body>
+		<div id="map" class="map"></div>
+		<script>
+			var map = new ol.Map({
+				layers: [
+					new ol.layer.Tile({
+						source: new ol.source.XYZ({
+							url: {{.TilePrefix}} + "{z}/{x}/{y}"
+						})
+					})
+				],
+				target: 'map',
+				view: new ol.View({
+					center: [0, 0],
+					zoom: 0
+				})
+			});
+		</script>
+	</body>
+</html>
+`))
 )
 
 var (
 	addr = flag.String("addr", "localhost:8080", "addr")
 	dsn  = flag.String("dsn", "", "dsn")
 )
+
+type mapServer struct {
+	TilePrefix string
+}
+
+func (ms *mapServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := indexHTML.Execute(w, ms); err != nil {
+		log.Print(err)
+	}
+}
 
 func run() error {
 	flag.Parse()
@@ -23,8 +65,14 @@ func run() error {
 		return err
 	}
 	defer mbt.Close()
-	mbtPrefix := "/" + filepath.Base(*dsn) + "/"
-	http.Handle(mbtPrefix, handlers.LoggingHandler(os.Stdout, http.StripPrefix(mbtPrefix, mbt)))
+	r := mux.NewRouter()
+	tilePrefix := "/" + filepath.Base(*dsn) + "/"
+	ms := &mapServer{
+		TilePrefix: tilePrefix,
+	}
+	r.PathPrefix(tilePrefix).Handler(http.StripPrefix(tilePrefix, mbt))
+	r.PathPrefix("/").Handler(http.StripPrefix("/", ms))
+	http.Handle("/", handlers.LoggingHandler(os.Stdout, r))
 	return http.ListenAndServe(*addr, nil)
 }
 
