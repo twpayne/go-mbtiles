550c7e5c191815e43fec9a0a78ab393dbb6f9caa
Joe Polastre <joe.polastre@flightaware.com>
2023-08-09 23:53:27 +0000

    Enhance sqlite mbtiles writer * Create, delete metadata tables * Create, drop tiles index, which doesn't need to be created until after imports are done * Add a BulkInsertTile function * Add a SetOptimizations function

--- writer.go
+++ writer.go
@@ -3,6 +3,7 @@ package mbtiles
 // FIXME use views
 
 import (
+	"context"
 	"database/sql"
 )
 
@@ -10,7 +11,15 @@ import (
 type Writer struct {
 	Reader
 	hasTiles       bool
+	hasMetadata    bool
 	tileInsertStmt *sql.Stmt
+}
+
+type Optimizations struct {
+	// Synchronous turns ON or OFF the statement PRAGMA synchronous = OFF
+	SynchronousOff bool
+	// JournalModeMemory turns ON or OFF the statement PRAGMA journal_mode = MEMORY
+	JournalModeMemory bool
 }
 
 // NewWriter returns a new Writer.
@@ -36,7 +45,22 @@ func (w *Writer) Close() error {
 	return err
 }
 
-// CreateTiles creates the tiles view if it does not already exist.
+// SetOptimizations can be used to turn on or off Optimization options
+func (w *Writer) SetOptimizations(opts Optimizations) error {
+	if opts.SynchronousOff {
+		if _, err := w.db.Exec("PRAGMA synchronous = OFF"); err != nil {
+			return err
+		}
+	}
+	if opts.JournalModeMemory {
+		if _, err := w.db.Exec("PRAGMA journal_mode = MEMORY"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// CreateTiles creates the tiles table if it does not already exist.
 func (w *Writer) CreateTiles() error {
 	if w.hasTiles {
 		return nil
@@ -48,6 +72,7 @@ func (w *Writer) CreateTiles() error {
 			tile_column INT NOT NULL,
 			tile_row INT NOT NULL,
 			tile_data BLOB NOT NULL
+			PRIMARY KEY (zoom_level, tile_column, tile_row)
 		);
 		CREATE UNIQUE INDEX IF NOT EXISTS tiles_index ON tiles (zoom_level, tile_column, tile_row);
 		COMMIT;
@@ -55,6 +80,59 @@ func (w *Writer) CreateTiles() error {
 		return err
 	}
 	w.hasTiles = true
+	return nil
+}
+
+// CreateTileIndex generates the standard index on the tiles table
+func (w *Writer) CreateTileIndex() error {
+	if _, err := w.db.Exec(`
+		BEGIN TRANSACTION;
+		CREATE UNIQUE INDEX IF NOT EXISTS tiles_index ON tiles (zoom_level, tile_column, tile_row);
+		COMMIT;
+	`); err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteTileIndex removes the tile index, useful for speeding up bulk inserts
+func (w *Writer) DeleteTileIndex() error {
+	if _, err := w.db.Exec(`
+		BEGIN TRANSACTION;
+		DROP INDEX IF EXISTS tiles_index;
+		COMMIT;
+	`); err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateMetadata creates the metadata table if it does not already exist.
+func (w *Writer) CreateMetadata() error {
+	if w.hasMetadata {
+		return nil
+	}
+	if _, err := w.db.Exec(`
+		BEGIN TRANSACTION;
+		CREATE TABLE IF NOT EXISTS metadata (name TEXT, value TEXT, PRIMARY KEY (name));
+		COMMIT;
+	`); err != nil {
+		return err
+	}
+	w.hasMetadata = true
+	return nil
+}
+
+// DeleteMetadata removes the metadata table, useful for resetting the metadata in the mbtiles file
+func (w *Writer) DeleteMetadata() error {
+	if _, err := w.db.Exec(`
+		BEGIN TRANSACTION;
+		DELETE FROM metadata;
+		COMMIT;
+	`); err != nil {
+		return err
+	}
+	w.hasMetadata = false
 	return nil
 }
 
@@ -72,6 +150,34 @@ func (w *Writer) InsertTile(z, x, y int, tileData []byte) error {
 	}
 	_, err := w.tileInsertStmt.Exec(z, x, y, tileData)
 	return err
+}
+
+// BulkInsertTile inserts multiple tiles at the coordinates provided (z, x, y).
+// This can be faster because it reduces the number of transactions.
+// By default, sqlite wraps each insert in a transaction.
+func (w *Writer) BulkInsertTile(z, x, y []int, tileData [][]byte) error {
+	if err := w.CreateTiles(); err != nil {
+		return err
+	}
+	tx, err := w.db.BeginTx(context.Background(), nil)
+	if err != nil {
+		return err
+	}
+	if w.tileInsertStmt == nil {
+		var err error
+		w.tileInsertStmt, err = w.db.Prepare("INSERT OR REPLACE INTO tiles (zoom_level, tile_column, tile_row, tile_data) VALUES (?, ?, ?, ?);")
+		if err != nil {
+			return err
+		}
+	}
+	stmt := tx.Stmt(w.tileInsertStmt)
+	for i, _ := range z {
+		if _, err := stmt.Exec(z[i], x[i], y[i], tileData[i]); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	return tx.Commit()
 }
 
 // SelectTile returns the tile at (z, x, y).
