aff7d30e07ef71f97fe1285ca3604875dff42772
Joe Polastre <joe.polastre@flightaware.com>
2023-08-14 23:27:54 +0000

    move metadata structs to types.go, make values pointers to differentiate when they're set

--- writer.go
+++ writer.go
@@ -32,24 +32,6 @@ type Optimizations struct {
 	JournalModeMemory bool
 }
 
-// MetadataJson is the metadata required by the mbtiles spec to be present for vector
-// tiles in the metadata table. This struct should be marshaled to a UTF-8 string
-// and the value stored with key name `json`.
-//
-// See https://github.com/mapbox/mbtiles-spec/blob/master/1.3/spec.md#vector-tileset-metadata
-type MetadataJson struct {
-	VectorLayers []MetadataJsonVectorLayer `json:"vector_layers"` // Defines the vector layers in this mbtiles file. Required for vector tilesets (mvt/pbf). Not necessary for raster image (png/webp/jpg/etc) mbtiles files.
-}
-
-// MetadataJsonVectorLayer contains information about each vector tile layer in this mbtiles file.
-type MetadataJsonVectorLayer struct {
-	ID          string            `json:"id"`                    // The layer ID, which is referred to as the name of the layer in the Mapbox Vector Tile spec.
-	Description string            `json:"description,omitempty"` // A human-readable description of the layer's contents.
-	MinZoom     int               `json:"minzoom,omitempty"`     // The lowest zoom level whose tiles this layer appears in.
-	MaxZoom     int               `json:"maxzoom,omitempty"`     // The highest zoom level whose tiles this layer appears in.
-	Fields      map[string]string `json:"fields"`                // Fields has keys and values are the names and types of attributes available in this layer. Each type MUST be the string "Number", "Boolean", or "String". Attributes whose type varies between features SHOULD be listed as "String".
-}
-
 // NewWriter returns a new Writer.
 func NewWriter(dsn string) (*Writer, error) {
 	r, err := NewReader(dsn)
